73ad6cf11c918708290b0e2d14dbe4211167c323
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-10 18:05:08 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -37,7 +37,7 @@ loghandler = {
         creator: `${creator}`,
         code: 406,
         message: 'masukan parameter apikey',
-        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6283898698875 , key nya gratis kok gan, jadi santuyy ajaa'
+        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/62858123730111 , key nya gratis kok gan, jadi santuyy ajaa'
     },
     notkey: {
         status: false,
@@ -103,7 +103,7 @@ loghandler = {
         status: false,
         creator: `${creator}`,
         code: 406,
-        message: 'apikey invalid, gak punya apikey? chat gw aja yaaa di wa.me/6283898698875 , key nya gratis kok gan, jadi santuyy ajaa'
+        message: 'apikey invalid, gak punya apikey? chat gw aja yaaa di wa.me/62858123730111 , key nya gratis kok gan, jadi santuyy ajaa'
     },
     invalidlink: {
         status: false,
